9d794957c5a6ebe3e47bf9798f6ae6eafe417f1a
GoogleSensei <gooooglesensei@gmail.com>
2021-08-17 19:09:06 +0900

    allow to access s3 and get object

--- index.js
+++ index.js
@@ -1,46 +1,18 @@
-var AWS = require('aws-sdk');
-aws.config.region = 'ap-north-east-1';
-var s3 = new AWS.S3();
-
-const paramsToGet = {
-    Bucket: 'taylor-transcribe-bucket',
-    Key: 'audio.json'
-};
-
-const paramsToPut = body => {
-    return {
-        Bucket: 'taylor-transcribe-bucket',
-        Key: 'audio.json',
-        Body: JSON.stringify(body),
-        ContentType: 'application/json',
-        ACL: 'public-read-write'
-    }
-
-};
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3({'region':'ap-northeast-1'});
 
 exports.handler = async (event, context, callback) => {
-
-    const res = {
-        statusCode: 200,
-    }    // s3.putObject(paramsToPut(JSON.stringify({ name: 'Mai Shiraishi', age: 25 })), (err, data) => {
-    //     if (err) {
-    //         console.log(err, err.stack);
-    //     }
-    //     context.done();
-    // });
-
-
-    // Write JSON File to S3
-
-
     // Read File from S3
-    s3.getObject(paramsToGet, (err, data) => {
-        if (err) {
-            console.log(err);
-            callback(err);
-        }
-        res.body = data.Body.toString();
-        callback(null, JSON.stringify(res));
-    });
+
+    const data = await s3.getObject(
+        {
+            Bucket:'taylor-transcribe-bucket',
+            Key:'audio.json'
+        }).promise()
+
+    return {
+        statusCode: 200,
+        obj: JSON.parse(data.Body),
+    }
 
 };
