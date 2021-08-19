f456563b0788421ccb13ba703470ad16f98e0cb4
GoogleSensei <gooooglesensei@gmail.com>
2021-08-19 13:54:41 +0900

    commit before refactoring.

--- index.js
+++ index.js
@@ -2,39 +2,31 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3({'region':'ap-northeast-1'});
 
 exports.handler = async (event, context, callback) => {
-    // Read File from S3
 
+    // URLパラメータの格納
+    const { word1, word2, word3 } = event?.queryStringParameters || {};
+    const wordArray = {words1:word1, words2:word2, words3:word3};
+    const Array = [word1, word2, word3];
+
+    console.log(wordArray)
+
+    // テキスト化した音声ファイルの取得
     const data = await s3.getObject(
         {
             Bucket:'taylor-transcribe-bucket',
             Key:'audio.json'
-        }).promise()
+        }).promise();
+    const { text } = JSON.parse(data.Body);
 
-    return {
-        statusCode: 200,
-        obj: JSON.parse(data.Body),
-    }
-
-};
-
-
-
-    const word1 = "条件";
-    const word2 = "文";
-    const word3 = "実行";
-    const wordArray = {words1:"条件", words2:"文", words3:"実行"};
-    const result = [word1, word2, word3];
-    const hogehoge = {};
-    console.log(result)
-
-    const hoge = result.map(word => {
-
-
+    // 検索処理
+    Array.map(word => {
+        if (word === undefined) {
+            return;
+        }
         let place = 0;
         const wordPlace=[];
         const words = [];
-        console.log(word);
-        const text = "条件式が true の場合、文が再び実行されます。文の実行終了時に毎回条件がチェックされます。条件文によってはああああ"
+
         // 検索結果の上位10個を取り出す。
         do {
             place = text.indexOf(word, place);
@@ -43,41 +35,31 @@ exports.handler = async (event, context, callback) => {
             }
             wordPlace.push(place);
             console.log(place);
-            place++
+            place++;
         } while (wordPlace.length < 10);
         console.log(wordPlace);
 
         // 配列の1つ目から前後５文字で単語を取得する
         wordPlace.map(number => {
             if (number < 5) {
-                words.push(text.substr(number, 5));
+                words.push(text.substr(number, word.length+5));
+            } else {
+                words.push(text.substr(number - 5, word.length+10));
             }
-            words.push(text.substr(number - 5, 10));
-        })
-        console.log(words);
+        });
 
         // 取得した単語と単語の位置を配列に詰める
         let resultData = {};
         for (let i = 0; i < 10; i++) {
-            if (words[i] === undefined) {
+            if (words[i] === undefined || wordPlace[i] === undefined) {
                 break;
             }
             resultData["word"] = word;
             resultData["around" + `${i}`] = words[i];
-            resultData["where" + `${i}`] = `${wordPlace[i]} words from beginning.`
+            resultData["where" + `${i}`] = `${wordPlace[i]} words from beginning.`;
         }
 
-        console.log(`文字列:${words}`)
-        console.log(`開始位置:${wordPlace}`)
-
-        // const fuga = result.map(words => {
-        //     if( words === word) {
-        //         return words.resultData = resultData;
-        //     } else {
-        //         return;
-        //     }
-        // })
-
+        // 対応する単語へ追加する
         if ( word === wordArray.words1) {
             wordArray.words1 = resultData;
         } else if ( word === wordArray.words2) {
@@ -85,10 +67,17 @@ exports.handler = async (event, context, callback) => {
         } else if ( word === wordArray.words3) {
             wordArray.words3 = resultData;
         }
+    });
 
-        console.log(JSON.stringify(resultData));
-        return resultData
-    })
-console.log(hoge)
-console.log(wordArray)
+    wordArray.text = text;
+    console.log(wordArray);
 
+    const response = {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*"
+        },
+        body: JSON.stringify(wordArray),
+    };
+    return response;
+};
